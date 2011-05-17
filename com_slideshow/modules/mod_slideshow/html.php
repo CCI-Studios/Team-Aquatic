6dfcf85564cf383ad6594d9f246bf55b1bf6700f
Jonathan Bennett <jbennett@ccistudios.com>
2011-05-17 13:42:12 -0400

    Completed slideshow module

--- com_slideshow/modules/mod_slideshow/html.php
+++ com_slideshow/modules/mod_slideshow/html.php
@@ -1,9 +1,17 @@
 <?php
+jimport('joomla.filesystem.folder');
 
 class ModSlideshowHtml extends ModDefaultHtml
 {
 	public function display()
 	{
+		$imagePath = JPATH_BASE .'/images/stories/'. $this->params->get('path') .'/';
+		$images = JFolder::files($imagePath, '.jpg');
+		
+		JHTML::stylesheet('site.css', 'media/mod_slideshow/');
+		JHTML::script('site.js', 'media/mod_slideshow/', true);
+		
+		$this->assign('images', $images);
 		return parent::display();
 	}
 }
--- com_slideshow/modules/mod_slideshow/tmpl/default.php
+++ com_slideshow/modules/mod_slideshow/tmpl/default.php
@@ -1,3 +1,29 @@
 <div class="mod_slideshow">
+	<div class="inner">
+		<? if ($params->get('title') || $params->get('description')): ?>
+			<div class="details">
+			<? if ($params->get('title')): ?>
+				<div class="title"><?= $params->get('title') ?></div>
+			<? endif; ?>
+			<? if ($params->get('description')): ?>
+				<div class="description"><?= $params->get('description') ?></div>
+			<? endif; ?>
+			</div>
+		<? endif; ?>
 	
+		<div class="images">
+			<? foreach ($images as $index=>$image): ?>
+			<div class="image <?= ($index == 0)? 'current':'' ?>">
+				<img src="/images/stories/<?= $params->get('path') .'/'. $image?>" />
+			</div>
+			<? endforeach; ?>
+		</div>
+		
+		<div class="clear"></div>
+	</div>
+	<div class="indicators">
+		<? foreach ($images as $index=>$image): ?>
+			<div class="indicator <?= ($index == 0)? 'current':'' ?>">&nbsp;</div>
+		<? endforeach; ?>
+	</div>
 </div>
