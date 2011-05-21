b14e0e15ea586380f3411c591f922ae7237b375e
Jonathan Bennett <jbennett@ccistudios.com>
2011-05-21 01:59:18 -0400

    Updated for Nooku

--- com_slideshow/modules/mod_slideshow/mod_slideshow.php
+++ com_slideshow/modules/mod_slideshow/mod_slideshow.php
@@ -2,7 +2,7 @@
 defined('_JEXEC') or die();
 defined('KOOWA') or die();
 
-KFactory::tmp('site::mod.slideshow.html', array(
+echo KFactory::tmp('site::mod.slideshow.html', array(
 	'params' 	=> $params,
 	'module'	=> $module,
 	'attribs'	=> $attribs
