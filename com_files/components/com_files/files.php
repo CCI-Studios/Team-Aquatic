7955ab3621963b74810fba4ec7d61eb77960ae1d
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-30 12:13:03 -0400

    Removed error checking

--- com_files/components/com_files/files.php
+++ com_files/components/com_files/files.php
@@ -2,7 +2,4 @@
 defined('_JEXEC') or die;
 defined('KOOWA') or die;
 
-error_reporting(E_ALL);			// FIXME 
-ini_set('display_errors', '1');
-
 echo KFactory::get('site::com.files.dispatcher')->dispatch();
