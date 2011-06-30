277d8a78d79a290466f2b590d62ca605090e228f
Jonathan Bennett <jbennett@ccistudios.com>
2011-06-30 11:06:19 -0400

    Added error reporting and component dispatcher

--- com_files/components/com_files/files.php
+++ com_files/components/com_files/files.php
@@ -2,4 +2,7 @@
 defined('_JEXEC') or die;
 defined('KOOWA') or die;
 
-echo "Adsf";
+error_reporting(E_ALL);			// FIXME 
+ini_set('display_errors', '1');
+
+echo KFactory::get('site::com.files.dispatcher')->dispatch();
